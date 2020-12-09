849cf7f5346079311061c7f7363946d518d183dc
t.akasaki <t.akasaki.csharp@gmail.com>
2020-12-10 00:18:08 +0900

    fix(lib): :bug: indexでのexport漏れを修正

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from './HttpMethod';
 export * from './HttpMethodController';
+export * from './utils/ToBeMethodAuthentication';
 export * from './utils/ToBeMethodDefied';
+export * from './utils/ToBeMethodFunction';
 export * from './Validation';
 export * from './Validator';
 
