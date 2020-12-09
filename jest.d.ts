1d8220cd95d3c04c8b71dbeeaca9c679112cda49
t.akasaki <t.akasaki.csharp@gmail.com>
2020-12-10 00:14:57 +0900

    test(lib): :sparkles: HttpMethodControllerで指定したHttpMethodが指定された関数が設定されているかのMatchersを実装

--- jest.d.ts
+++ jest.d.ts
@@ -3,5 +3,6 @@ declare namespace jest {
   interface Matchers<R> {
     toBeMethodDefied(method: import("./src/HttpMethod").HttpMethod): R;
     toBeMethodAuthentication(method: import("./src/HttpMethod").HttpMethod): R;
+    toBeMethodFunction(method: import("./src/HttpMethod").HttpMethod, funcName: string);
   }
 }
